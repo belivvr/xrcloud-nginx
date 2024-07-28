a586566bce78b129616f82404f4c7c834e1e33b9
Ubuntu <azureuser@room-duphonics-co.vaypqspribxe1nqdjcwmlxmpkh.ix.internal.cloudapp.net>
2024-07-28 09:26:16 +0000

    add xrcloud nginx option at restatr server

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
+sudo systemctl stop nginx
 sudo docker stop xrcloud-nginx
 sudo docker rm xrcloud-nginx
 sudo docker build -t xrcloud-nginx .
-sudo docker run -d --name xrcloud-nginx --network xrcloud -p 80:80 -p 443:443 -v /home/belivvr/xrcloud-nginx/ssl:/etc/ssl -v /home/belivvr/xrcloud-nginx/nginx.conf:/etc/nginx/nginx.conf -v /app/xrcloud-backend/storage:/app/xrcloud-backend/storage xrcloud-nginx:latest
+sudo docker run -d --name xrcloud-nginx --restart always --network xrcloud -p 80:80 -p 443:443 -v /home/azureuser/xrcloud-nginx/ssl:/etc/ssl -v /home/azureuser/xrcloud-nginx/nginx.conf:/etc/nginx/nginx.conf -v /app/xrcloud-backend/storage:/app/xrcloud-backend/storage xrcloud-nginx:latest
